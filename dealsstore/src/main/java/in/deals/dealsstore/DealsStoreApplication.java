115965da158a73bebc862d83bce886b6947a41e6
computerwala <shivsoni377@gmail.com>
2021-04-07 23:49:47 +0530

    initial commit. Done with spring boot application and configure the application properties

---
+++ dealsstore/src/main/java/in/deals/dealsstore/DealsStoreApplication.java
@@ -0,0 +1,13 @@
+package in.deals.dealsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealsStoreApplication.class, args);
+	}
+
+}
---
+++ dealsstore/src/test/java/in/deals/dealsstore/DealsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.deals.dealsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
